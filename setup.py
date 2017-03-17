50e9c66d616409a570efff8c3cb225b3c41c733e
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2017-03-17 20:30:22 +0000

    add setup.py remove redundant test

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(name='Robot Daemon',
+      version='1.0',
+      description='Daemon for vision code for Source Bots',
+      author='SourceBots',
+      author_email='',
+      packages=['robotd'],
+      setup_requires=["cffi>=1.0.0"],
+      ffi_modules=["robotd/vision/apriltag/apriltag_build.py:ffibuilder"],
+      install_requires=['pyudev', 'pyserial', 'pygame', 'Pillow', "cffi>=1.0.0", 'numpy'],
+     )
