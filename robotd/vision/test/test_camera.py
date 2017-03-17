0a6e9874abf4e6f1d4442ed9c2e614a35af6c07e
Alistair Lynn <arplynn@gmail.com>
2017-03-17 20:55:37 +0000

    This is now a `RuntimeError`

--- robotd/vision/test/test_camera.py
+++ robotd/vision/test/test_camera.py
@@ -1,11 +1,11 @@
 import unittest
 
-from robotd.vision.camera import Camera, CameraError
+from robotd.vision.camera import Camera
 
 
 class CameraTestCase(unittest.TestCase):
     def test_invalid_camera(self):
         """ Test the camera errors when reading from an invalid camera"""
         cam = Camera("/", (1, 1), 1)
-        with self.assertRaises(CameraError):
+        with self.assertRaises(RuntimeError):
             cam.init()
