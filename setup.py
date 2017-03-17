1f77431db0c0183dcc7fc0ff657057b89fa1bb48
Alistair Lynn <arplynn@gmail.com>
2017-03-17 21:02:10 +0000

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,26 @@
 from setuptools import setup, find_packages
 
 
-setup(name='Robot Daemon',
-      version='1.0',
-      description='Daemon for vision code for Source Bots',
-      author='SourceBots',
-      author_email='',
-      packages=find_packages(),
-      setup_requires=["cffi>=0.8.6"],
-      ffi_modules=["robotd/vision/apriltag/apriltag_build.py:ffibuilder"],
-      install_requires=['pyudev', 'pyserial', 'pygame', 'Pillow', "cffi>=0.8.6", 'numpy', 'setproctitle'],
-     )
+setup(
+    name='robotd',
+    version='1.0',
+    description="Daemon for vision code for Source Bots",
+    author="SourceBots",
+    author_email='',
+    packages=find_packages(),
+    setup_requires=[
+        'cffi>=0.8.6',
+    ],
+    ffi_modules=[
+        'robotd/vision/apriltag/apriltag_build.py:ffibuilder',
+    ],
+    install_requires=[
+        'pyudev',
+        'pyserial',
+        'pygame',
+        'Pillow',
+        "cffi>=0.8.6",
+        'numpy',
+        'setproctitle',
+    ],
+)
