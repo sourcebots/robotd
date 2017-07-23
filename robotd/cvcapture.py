6e59fc28d9fbbfbe05da1cd75131b090cf263398
Alistair Lynn <arplynn@gmail.com>
2017-07-23 19:28:39 +0100

    Delete the old implementation

--- robotd/cvcapture.py
+++ robotd/cvcapture.py
@@ -48,18 +48,3 @@ class CaptureDevice(object):
             self.instance = None
 
     __del__ = close
-
-def cvcapture(device, width, height):
-    capture_buffer = _cvcapture.ffi.new('uint8_t[{}]'.format(width * height))
-    print(capture_buffer)
-
-    result = _cvcapture.lib.cvcapture(
-        capture_buffer,
-        width,
-        height,
-    )
-
-    if result == 0:
-        raise RuntimeError("cvcapture() failed")
-
-    return bytes(_cvcapture.ffi.buffer(capture_buffer))
