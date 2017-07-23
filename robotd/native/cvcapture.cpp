07767e0eda39eb68534b712e72720f81bdc2380b
Alistair Lynn <arplynn@gmail.com>
2017-07-23 16:34:31 +0100

    Run adaptive thresholding

--- robotd/native/cvcapture.cpp
+++ robotd/native/cvcapture.cpp
@@ -43,7 +43,7 @@ int cvcapture(void* context, void* buffer, size_t width, size_t height) {
         return 0;
     }
 
-    cv::Mat colour_image, greyscale_image, denoised_image;
+    cv::Mat colour_image, greyscale_image, denoised_image, thresholded_image;
 
     if (!cap->isOpened()) {
         return 0;
@@ -77,9 +77,21 @@ int cvcapture(void* context, void* buffer, size_t width, size_t height) {
     if (died_horribly) {
         return 0;
     }
+    size_t kernel_size = (width < height) ? width : height;
+    kernel_size /= 2;
+    kernel_size |= 1;
+    cv::adaptiveThreshold(
+        denoised_image,
+        thresholded_image,
+        255.0,
+        CV_ADAPTIVE_THRESH_MEAN_C,
+        CV_THRESH_BINARY,
+        kernel_size,
+        0.0
+    );
     memcpy(
         buffer,
-        denoised_image.ptr(),
+        thresholded_image.ptr(),
         width * height
     );
     return 1;
