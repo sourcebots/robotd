sourcebots/robotd

commit b065ce1503c62a58ab4991516b94246a32ad3dc8

Rework

robotd/master.py
@@ -21,11 +21,12 @@ class BoardRunner(multiprocessing.Process):
         """Constructor from a given `Board`."""
         super().__init__(**kwargs)
         self.board = board
-        self.socket_path = Path("{}robotd/{}/{}".format(
-            root_dir,
-            type(board).board_type_id,
-            board.name(board.node),
-        ))
+        self.socket_path = (
+            self.root_dir /
+            'robotd' /
+            type(board).board_type_id /
+            board.name(board.node)
+        )
         try:
             self.socket_path.parent.mkdir(parents=True)
         except FileExistsError:

commit 98d095a5ed9cec111ff0559e282120f40fda0082

Fix regression introduced in b065ce1503c62a58ab4991516b94246a32ad3dc8

robotd/master.py
@@ -22,7 +22,7 @@ class BoardRunner(multiprocessing.Process):
         super().__init__(**kwargs)
         self.board = board
         self.socket_path = (
-            self.root_dir /
+            root_dir /
             type(board).board_type_id /
             board.name(board.node)
         )